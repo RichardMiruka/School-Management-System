8d4302247c07e9f54dffbbda82d75752248d9974
RichardMiruka <richardonsare15@gmail.com>
2024-05-24 17:09:13 +0300

    refactoring to use Express routing

--- app/app.js
+++ app/app.js
@@ -33,244 +33,263 @@ app.use("/api/v1/admins/register", adminRouter);
 // });
 
 // admin login
-app.post("/api/v1/admins/login", (req, res) => {
-    try {
-        res.status(201).json({
-            status: "success",
-            data : "Admin logged in successfully"
-        })
-    } catch (error) {
-        res.status(400).json({
-            status: "failed",
-            message: error.message
-        })
-    }
+app.use("/api/v1/admins/login", adminRouter);
+// app.post("/api/v1/admins/login", (req, res) => {
+//     try {
+//         res.status(201).json({
+//             status: "success",
+//             data : "Admin logged in successfully"
+//         })
+//     } catch (error) {
+//         res.status(400).json({
+//             status: "failed",
+//             message: error.message
+//         })
+//     }
     
-});
+// });
 
 //admin logout
-app.post("/api/v1/admins/logout", (req, res) => {
-    try {
-        res.status(201).json({
-            status: "success",
-            message: "Admin logged out successfully"
-        })
-    } catch (error) {
-        res.status(400).json({
-            status: "failed",
-            message: error.message
-        })
-    }
+// app.post("/api/v1/admins/logout", (req, res) => {
+//     try {
+//         res.status(201).json({
+//             status: "success",
+//             message: "Admin logged out successfully"
+//         })
+//     } catch (error) {
+//         res.status(400).json({
+//             status: "failed",
+//             message: error.message
+//         })
+//     }
     
-});
+// });
 
 // get all admins
-app.get("/api/v1/admins/", (req, res) => {
-    try {
-        res.status(201).json({
-            status: "success",
-            message: "All admins fetched successfully"
-        })
-    } catch (error) {
-        res.status(400).json({
-            status: "failed",
-            message: error.message
-        })
-    }
+app.use("/api/v1/admins/", adminRouter);
+// app.get("/api/v1/admins/", (req, res) => {
+//     try {
+//         res.status(201).json({
+//             status: "success",
+//             message: "All admins fetched successfully"
+//         })
+//     } catch (error) {
+//         res.status(400).json({
+//             status: "failed",
+//             message: error.message
+// //         })
+// //     }
     
-});
+// });
 
 // get single admin
-app.get("/api/v1/admins/:id", (req, res) => {
-    try {
-        res.status(201).json({
-            status: "success",
-            message: "Admin fetched successfully"
-        })
-    } catch (error) {
-        res.status(400).json({
-            status: "failed",
-            message: error.message
-        })
-    }
+app.use("/api/v1/admins/:id", adminRouter);
+// app.get("/api/v1/admins/:id", (req, res) => {
+//     try {
+//         res.status(201).json({
+//             status: "success",
+//             message: "Admin fetched successfully"
+//         })
+//     } catch (error) {
+//         res.status(400).json({
+//             status: "failed",
+//             message: error.message
+//         })
+//     }
     
-});
+// });
 
 // update admin
-app.put("/api/v1/admins/:id", (req, res) => {
-    try {
-        res.status(201).json({
-            status: "success",
-            message: "Admin updated successfully"
-        })
-    } catch (error) {
-        res.status(400).json({
-            status: "failed",
-            message: error.message
-        })
-    }
+ app.use("/api/v1/admins/:id", adminRouter);
+// app.put("/api/v1/admins/:id", (req, res) => {
+//     try {
+//         res.status(201).json({
+//             status: "success",
+//             message: "Admin updated successfully"
+//         })
+//     } catch (error) {
+//         res.status(400).json({
+//             status: "failed",
+//             message: error.message
+//         })
+//     }
     
-});
+// });
 
 // delete admin
-app.delete("/api/v1/admins/:id", (req, res) => {
-    try {
-        res.status(201).json({
-            status: "success",
-            message: "Admin deleted successfully"
-        })
-    } catch (error) {
-        res.status(400).json({
-            status: "failed",
-            message: error.message
-        })
-    }
+app.use("/api/v1/admins/:id", adminRouter);
+
+// app.delete("/api/v1/admins/:id", (req, res) => {
+//     try {
+//         res.status(201).json({
+//             status: "success",
+//             message: "Admin deleted successfully"
+//         })
+//     } catch (error) {
+//         res.status(400).json({
+//             status: "failed",
+//             message: error.message
+//         })
+//     }
     
-});
+// });
 
 // admin suspending a teacher account
-app.put("/api/v1/admins/suspend/teacher/:id", (req, res) => {
-    try {
-        res.status(201).json({
-            status: "success",
-            message: "Teacher account suspended successfully"
-        })
-    } catch (error) {
-        res.status(400).json({
-            status: "failed",
-            message: error.message
-        })
-    }
+app.use ("/api/v1/admins/suspend/teacher/:id", adminRouter);
+// app.put("/api/v1/admins/suspend/teacher/:id", (req, res) => {
+//     try {
+//         res.status(201).json({
+//             status: "success",
+//             message: "Teacher account suspended successfully"
+//         })
+//     } catch (error) {
+//         res.status(400).json({
+//             status: "failed",
+//             message: error.message
+//         })
+//     }
     
-});
+// });
 
 // admin unsuspending a teacher account
-app.put("/api/v1/admins/unsuspend/teacher/:id", (req, res) => {
-    try {
-        res.status(201).json({
-            status: "success",
-            message: "Teacher account unsuspended successfully"
-        })
-    } catch (error) {
-        res.status(400).json({
-            status: "failed",
-            message: error.message
-        })
-    }
+app.use ("/api/v1/admins/unsuspend/teacher/:id", adminRouter);
+
+// app.put("/api/v1/admins/unsuspend/teacher/:id", (req, res) => {
+//     try {
+//         res.status(201).json({
+//             status: "success",
+//             message: "Teacher account unsuspended successfully"
+//         })
+//     } catch (error) {
+//         res.status(400).json({
+//             status: "failed",
+//             message: error.message
+//         })
+//     }
     
-});
+// });
 
 // admin withdrawing a teacher account
-app.put("/api/v1/admins/withdraw/teacher/:id", (req, res) => {
-    try {
-        res.status(201).json({
-            status: "success",
-            message: "Teacher account withdrawn successfully"
-        })
-    } catch (error) {
-        res.status(400).json({
-            status: "failed",
-            message: error.message
-        })
-    }
+app.use ("/api/v1/admins/withdraw/teacher/:id", adminRouter);
+
+// app.put("/api/v1/admins/withdraw/teacher/:id", (req, res) => {
+//     try {
+//         res.status(201).json({
+//             status: "success",
+//             message: "Teacher account withdrawn successfully"
+//         })
+//     } catch (error) {
+//         res.status(400).json({
+//             status: "failed",
+//             message: error.message
+//         })
+//     }
     
-});
+// });
 
 // admin unwithdrawing a teacher account
-app.put("/api/v1/admins/unwithdraw/teacher/:id", (req, res) => {
-    try {
-        res.status(201).json({
-            status: "success",
-            message: "Teacher account unwithdrawn successfully"
-        })
-    } catch (error) {
-        res.status(400).json({
-            status: "failed",
-            message: error.message
-        })
-    }
+app.use ("/api/v1/admins/unwithdraw/teacher/:id", adminRouter);
+
+// app.put("/api/v1/admins/unwithdraw/teacher/:id", (req, res) => {
+//     try {
+//         res.status(201).json({
+//             status: "success",
+//             message: "Teacher account unwithdrawn successfully"
+//         })
+//     } catch (error) {
+//         res.status(400).json({
+//             status: "failed",
+//             message: error.message
+//         })
+//     }
     
-});
+// });
 
 // admin approving a teacher account
-app.put("/api/v1/admins/approve/teacher/:id", (req, res) => {
-    try {
-        res.status(201).json({
-            status: "success",
-            message: "Teacher account approved successfully"
-        })
-    } catch (error) {
-        res.status(400).json({
-            status: "failed",
-            message: error.message
-        })
-    }
+app.use ("/api/v1/admins/approve/teacher/:id", adminRouter);
+
+// app.put("/api/v1/admins/approve/teacher/:id", (req, res) => {
+//     try {
+//         res.status(201).json({
+//             status: "success",
+//             message: "Teacher account approved successfully"
+//         })
+//     } catch (error) {
+//         res.status(400).json({
+//             status: "failed",
+//             message: error.message
+//         })
+//     }
     
-});
+// });
 
 // admin rejecting a teacher account
-app.put("/api/v1/admins/reject/teacher/:id", (req, res) => {
-    try {
-        res.status(201).json({
-            status: "success",
-            message: "Teacher account rejected successfully"
-        })
-    } catch (error) {
-        res.status(400).json({
-            status: "failed",
-            message: error.message
-        })
-    }
+// app.put("/api/v1/admins/reject/teacher/:id", (req, res) => {
+//     try {
+//         res.status(201).json({
+//             status: "success",
+//             message: "Teacher account rejected successfully"
+//         })
+//     } catch (error) {
+//         res.status(400).json({
+//             status: "failed",
+//             message: error.message
+//         })
+//     }
     
-});
+// });
 
 // admin publishing exam results
-app.put("/api/v1/admins/publish/exam/:id", (req, res) => {
-    try {
-        res.status(201).json({
-            status: "success",
-            message: "Exam results published successfully"
-        })
-    } catch (error) {
-        res.status(400).json({
-            status: "failed",
-            message: error.message
-        })
-    }
+app.use("/api/v1/admins/publish/exam/:id", adminRouter);
+// app.put("/api/v1/admins/publish/exam/:id", (req, res) => {
+//     try {
+//         res.status(201).json({
+//             status: "success",
+//             message: "Exam results published successfully"
+//         })
+//     } catch (error) {
+//         res.status(400).json({
+//             status: "failed",
+//             message: error.message
+//         })
+//     }
     
-});
+// });
 
 // admin un-publishing exam results
-app.put("/api/v1/admins/unpublish/exam/:id", (req, res) => {
-    try {
-        res.status(201).json({
-            status: "success",
-            message: "Exam results unpublished successfully"
-        })
-    } catch (error) {
-        res.status(400).json({
-            status: "failed",
-            message: error.message
-        })
-    }
+app.use("/api/v1/admins/unpublish/exam/:id", adminRouter);
+// app.put("/api/v1/admins/unpublish/exam/:id", (req, res) => {
+//     try {
+//         res.status(201).json({
+//             status: "success",
+//             message: "Exam results unpublished successfully"
+//         })
+//     } catch (error) {
+//         res.status(400).json({
+//             status: "failed",
+//             message: error.message
+//         })
+//     }
     
-});
+// });
 
 // admin deleting exam results
-app.delete("/api/v1/admins/delete/exam/:id", (req, res) => {
-    try {
-        res.status(201).json({
-            status: "success",
-            message: "Exam results deleted successfully"
-        })
-    } catch (error) {
-        res.status(400).json({
-            status: "failed",
-            message: error.message
-        })
-    }
+app.use("/api/v1/admins/delete/exam/:id", adminRouter);
+
+// app.delete("/api/v1/admins/delete/exam/:id", (req, res) => {
+//     try {
+//         res.status(201).json({
+//             status: "success",
+//             message: "Exam results deleted successfully"
+//         })
+//     } catch (error) {
+//         res.status(400).json({
+//             status: "failed",
+//             message: error.message
+//         })
+//     }
     
-});
+// });
 
 
 module.exports = app; // export app
