1709919c2b215b1b4b07895a734ad0d24e1c5850
RichardMiruka <richardonsare15@gmail.com>
2024-06-06 10:54:21 +0300

    hashing the admin password

--- model/Staff/Admin.js
+++ model/Staff/Admin.js
@@ -1,29 +1,42 @@
-const mongoose = require("mongoose"); // import mongoose
-const adminSchema = new mongoose.Schema({ // create a schema, our blueprint for documents in this collection
+const bcrypt = require("bcryptjs");
 
-    name: {
-        type: String,
-        required: true
+const mongoose = require("mongoose");
+const adminSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        role: {
+            type: String,
+            default: "admin",
+        },
     },
-    email: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    role: {
-        type: String,
-        default: "admin"
-    },
-
-}, {
-    timestamps: true
-}
+    {
+        timestamps: true,
+    }
 );
 
-// model
-const Admin = mongoose.model("Admin", adminSchema);
-module.exports = Admin;
+//Hash password
+adminSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) {
+        next();
+    }
+    //salt
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
 
+//model
+const Admin = mongoose.model("Admin", adminSchema);
+
+module.exports = Admin;
