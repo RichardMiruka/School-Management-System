dfb3c8b4c38d9b6e00e7e6beee4cd9a7e703888f
RichardMiruka <richardonsare15@gmail.com>
2024-06-06 19:30:39 +0300

    Implemented protected API routes for managing Programs: create, retrieve (all and by ID), update, delete, and add subjects. Secured with isLogin and isAdmin middleware.

---
+++ routes/academics/program.js
@@ -0,0 +1,38 @@
+// Creating a separate router for Program management with protected routes
+// for CRUD operations on programs.
+
+const express = require("express");
+const {
+    createProgram,
+    deleteProgram,
+    getProgram,
+    getPrograms,
+    updatProgram,
+    addSubjectToProgram,
+} = require("../../controller/academics/programs");
+const isAdmin = require("../../middlewares/isAdmin");
+const isLogin = require("../../middlewares/isLogin");
+
+const programRouter = express.Router();
+
+// academicTerRouter.post("/", isLogin, isAdmin, createAcademicYear);
+// academicTerRouter.get("/", isLogin, isAdmin, getAcademicYears);
+
+programRouter
+    .route("/")
+    .post(isLogin, isAdmin, createProgram)
+    .get(isLogin, isAdmin, getPrograms);
+
+programRouter
+    .route("/:id")
+    .get(isLogin, isAdmin, getProgram)
+    .put(isLogin, isAdmin, updatProgram)
+    .delete(isLogin, isAdmin, deleteProgram);
+
+programRouter.put("/:id/subjects", isLogin, isAdmin, addSubjectToProgram);
+
+// academicTerRouter.get("/:id", isLogin, isAdmin, getAcademicYear);
+// academicTerRouter.put("/:id", isLogin, isAdmin, updateAcademicYear);
+// academicTerRouter.delete("/:id", isLogin, isAdmin, deleteAcademicYear);
+
+module.exports = programRouter;
