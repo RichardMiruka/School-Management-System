ba8d2fdaccef77b868810725593b3ccf326b9bf4
RichardMiruka <richardonsare15@gmail.com>
2024-06-06 19:23:31 +0300

    Created a separate router for Class Level management with protected routes for CRUD operations

---
+++ routes/academics/classLevel.js
@@ -0,0 +1,37 @@
+// Implementing protected API routes for managing Class Levels:
+// create, retrieve (all and by ID), update, and delete. 
+// Secured with isLogin and isAdmin middleware.
+
+const express = require("express");
+const {
+    createClassLevel,
+    deleteClassLevel,
+    getClassLevel,
+    getClassLevels,
+    updateclassLevel,
+} = require("../../controller/academics/classLevel");
+
+const isAdmin = require("../../middlewares/isAdmin");
+const isLogin = require("../../middlewares/isLogin");
+
+const classLevelRouter = express.Router();
+
+// academicTerRouter.post("/", isLogin, isAdmin, createAcademicYear);
+// academicTerRouter.get("/", isLogin, isAdmin, getAcademicYears);
+
+classLevelRouter
+    .route("/")
+    .post(isLogin, isAdmin, createClassLevel)
+    .get(isLogin, isAdmin, getClassLevels);
+
+classLevelRouter
+    .route("/:id")
+    .get(isLogin, isAdmin, getClassLevel)
+    .put(isLogin, isAdmin, updateclassLevel)
+    .delete(isLogin, isAdmin, deleteClassLevel);
+
+// academicTerRouter.get("/:id", isLogin, isAdmin, getAcademicYear);
+// academicTerRouter.put("/:id", isLogin, isAdmin, updateAcademicYear);
+// academicTerRouter.delete("/:id", isLogin, isAdmin, deleteAcademicYear);
+
+module.exports = classLevelRouter;
