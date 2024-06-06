ddc6d5b51fb65609013a77167b8b4bce2c8b1323
RichardMiruka <richardonsare15@gmail.com>
2024-06-06 13:39:23 +0300

    updated app.js with the middleware path'

--- app/app.js
+++ app/app.js
@@ -1,45 +1,23 @@
-const express = require("express");
+const express = require("express"); // import express
+
+const {
+    globalErrorHandler,
+    notFoundHandler,
+} = require("../middlewares/globalErrHandler");
+
 const adminRouter = require("../routes/staff/adminRouter");
 
 const app = express();
 
+
 //Middlewares
 app.use(express.json()); //pass incoming json data
 
-app.use((req, res, next) => {
-    console.log(`${req.method} ${req.originalUrl}`);
-    next();
-});
-
-let user = {
-    name: "John Does",
-    isAdmin: false,
-    isLogin: true,
-};
-
-const isLogin = (req, res, next) => {
-    if (user.isLogin) {
-        next();
-    } else {
-        res.status(401).json({
-            msg: "Unauthorized",
-        });
-    }
-};
-
-const isAdmin = (req, res, next) => {
-    if (user.isAdmin) {
-        next();
-    } else {
-        res.status(401).json({
-            msg: "Unauthorized, you are not admin",
-        });
-    }
-};
-
-app.use(isLogin, isAdmin);
 //Routes
-//admin register
 app.use("/api/v1/admins", adminRouter);
+
+//Error middlewares
+app.use(notFoundErr);
+app.use(globalErrHandler);
 
 module.exports = app;
