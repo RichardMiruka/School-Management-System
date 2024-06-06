dce1fbf4970894ffed8f4ba7e1ea17889ed1de3d
RichardMiruka <richardonsare15@gmail.com>
2024-06-06 17:58:20 +0300

    Initial server setup: Load environment variables, connect to database, start server on defined port.

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 require("dotenv").config(); // load environment variables from .env file
-const http = require("http");
+const http = require("http"); // import http module
 require("./config/dbConnect"); // connect to database
-const app = require("./app/app");
+const app = require("./app/app"); // import express application
 
 // set up server
 const PORT = process.env.PORT || 3000; // set port number for our application to listen to
 
 // server 
-const server = http.createServer(app);
-server.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+const server = http.createServer(app); // create http server using express app
+server.listen(PORT, () => console.log(`Server started on port ${PORT}`)); // start server and listen for incoming requests
