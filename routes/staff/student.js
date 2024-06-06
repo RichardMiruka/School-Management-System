d47eb00ae9d367682147cd69009b121974efc83d
RichardMiruka <richardonsare15@gmail.com>
2024-06-06 19:56:44 +0300

    Implemented secure API endpoints for comprehensive student management from registration, login, and profile management to exam writing (writeExam). Separate functionalities for Admin and Student users are implemented.

---
+++ routes/staff/student.js
@@ -0,0 +1,44 @@
+// Implement protected API routes for student management:
+// admin register, login, profile retrieval, update profile (student and admin),
+// write exam, retrieve all students (admin), and retrieve student by ID (admin).
+// Secured with appropriate middleware for authentication and authorization.
+
+const express = require("express");
+const {
+    adminRegisterStudent,
+    loginStudent,
+    getStudentProfile,
+    getAllStudentsByAdmin,
+    getStudentByAdmin,
+    studentUpdateProfile,
+    adminUpdateStudent,
+    writeExam,
+} = require("../../controller/students/studentsCtrl");
+
+const isAdmin = require("../../middlewares/isAdmin");
+const isLogin = require("../../middlewares/isLogin");
+const isStdudent = require("../../middlewares/isStudent");
+const isStudentLogin = require("../../middlewares/isStudentLogin");
+const studentRouter = express.Router();
+
+studentRouter.post("/admin/register", isLogin, isAdmin, adminRegisterStudent);
+studentRouter.post("/login", loginStudent);
+studentRouter.get("/profile", isStudentLogin, isStdudent, getStudentProfile);
+studentRouter.get("/admin", isLogin, isAdmin, getAllStudentsByAdmin);
+studentRouter.get("/:studentID/admin", isLogin, isAdmin, getStudentByAdmin);
+
+studentRouter.post(
+    "/exam/:examID/write",
+    isStudentLogin,
+    isStdudent,
+    writeExam
+);
+
+studentRouter.put("/update", isStudentLogin, isStdudent, studentUpdateProfile);
+studentRouter.put(
+    "/:studentID/update/admin",
+    isLogin,
+    isAdmin,
+    adminUpdateStudent
+);
+module.exports = studentRouter;
