13adfcdefdd0b6ded0fe06e379c9d1307515f538
RichardMiruka <richardonsare15@gmail.com>
2024-06-09 22:59:24 +0300

    tested all my endpoints to ensure they are protected and working as expected

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const http = require("http");
 require("./config/dbConnect");
 const app = require("./app/app");
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 //server
 const server = http.createServer(app);
 server.listen(PORT, console.log(`Server is running on port ${PORT}`));
